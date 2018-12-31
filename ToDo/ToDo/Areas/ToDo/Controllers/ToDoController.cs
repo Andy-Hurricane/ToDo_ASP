cdf8359bd461f94ca77cf9469d4bc399ec214ed3
Andy Hurricane <andrzejlopko@gmail.com>
2018-12-31 14:37:56 +0100

    Add homepage

---
+++ ToDo/ToDo/Areas/ToDo/Controllers/ToDoController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ToDo.Areas.ToDo.Controllers
+{
+    public class ToDoController : Controller
+    {
+        // GET: ToDo/ToDo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ToDo/ToDo/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ToDo/ToDo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ToDo/ToDo/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ToDo/ToDo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ToDo/ToDo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ToDo/ToDo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ToDo/ToDo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
