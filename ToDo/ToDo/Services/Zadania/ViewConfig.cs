b4a9cf1df511bea0d764b177172e95db446af1c9
Andrzej <andrzej@lumel.com.pl>
2019-01-15 12:19:33 +0100

    Add ViewConfig

---
+++ ToDo/ToDo/Services/Zadania/ViewConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Services.Zadania
+{
+    [NotMapped]
+    public class ViewConfig
+    {
+        public int ActualSite { get; set; } = 1;
+        public int PagePerSite { get; set; } = 10;
+    }
+}
