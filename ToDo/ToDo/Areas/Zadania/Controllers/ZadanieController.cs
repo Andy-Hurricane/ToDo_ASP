67e24d79d1521f95fc6312fe0536deb773e45a37
Andrzej <andrzej@lumel.com.pl>
2019-01-15 12:33:44 +0100

    Create Controler and Index view to Zadanie

---
+++ ToDo/ToDo/Areas/Zadania/Controllers/ZadanieController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ToDo.Areas.Zadania.Controllers
+{
+    public class ZadanieController : Controller
+    {
+        // GET: Zadania/Zadanie
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
