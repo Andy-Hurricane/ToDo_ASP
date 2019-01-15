8c651f8d2408ff6156367b6cf1131798043781dc
Andy Hurricane <andrzejlopko@gmail.com>
2019-01-15 21:09:29 +0100

    Add Service

--- ToDo/ToDo/Areas/Zadania/Controllers/ZadanieController.cs
+++ ToDo/ToDo/Areas/Zadania/Controllers/ZadanieController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ToDo.Services.Zadania;
 
 namespace ToDo.Areas.Zadania.Controllers
 {
     public class ZadanieController : Controller
     {
+        private IZadanieService Service {get;set;}
+
+        public ZadanieController(IZadanieService service)
+        {
+            Service = service;
+        }
+
         // GET: Zadania/Zadanie
         public ActionResult Index()
         {
---
+++ ToDo/ToDo/Services/Zadania/ZadanieService.svc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using ToDo.Areas.Zadania.Models;
+
+namespace ToDo.Services.Zadania
+{
+    // UWAGA: możesz użyć polecenia „Zmień nazwę” w menu „Refaktoryzuj”, aby zmienić nazwę klasy „ZadanieService” w kodzie, usłudze i pliku konfiguracji.
+    // UWAGA: aby uruchomić klienta testowego WCF w celu przetestowania tej usługi, wybierz plik ZadanieService.svc lub ZadanieService.svc.cs w eksploratorze rozwiązań i rozpocznij debugowanie.
+    public class ZadanieService : IZadanieService
+    {
+        private string Error { get; set; }
+
+        public void DoWork()
+        {
+        }
+
+        public IEnumerable<Task> GetTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Add(Task newTask)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Edit(Task editedTask)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SwapNext(Task swapMe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SwapPrevious(Task swapMe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetError()
+        {
+            return Error;
+        }
+    }
+}
