67cd850caefea7d61bf33aecfad7a772ea3e3ffd
Andrzej <andrzej@lumel.com.pl>
2019-01-15 12:23:20 +0100

    Add documentation to Task

--- ToDo/ToDo/Areas/Zadania/Models/Task.cs
+++ ToDo/ToDo/Areas/Zadania/Models/Task.cs
@@ -7,47 +7,77 @@ namespace ToDo.Areas.Zadania.Models
 {
     public class Task
     {
+        /// <summary>
+        /// Id zadania.
+        /// </summary>
         [Key]
         public int Id { get; set; }
         
+        /// <summary>
+        /// Temat zadania.
+        /// </summary>
         [Required(AllowEmptyStrings = false, ErrorMessage = "Temat nie może być pusty.")]
         [MaxLength(255, ErrorMessage = "Długość nie może przekraczać 255 znaków.")]
         [Index(IsUnique = true)]
         [DisplayName("Temat")]
         public string Topic { get; set; }
 
+        /// <summary>
+        /// Czynność zadania.
+        /// </summary>
         [Required(AllowEmptyStrings = false, ErrorMessage = "Czynność nie może być pusta.")]
         [MaxLength(255, ErrorMessage = "Długość nie może przekraczać 255 znaków.")]
         [DisplayName("Czynność")]
         public string Action { get; set; }
 
+        /// <summary>
+        /// Status zadania.
+        /// </summary>
         [Required(ErrorMessage = "Status nie może być pusty.")]
         [DisplayName("Status")]
         public int Status { get; set; }
 
+        /// <summary>
+        /// Data rozpoczęcia zadania.
+        /// </summary>
         [Required(ErrorMessage = "Data rozpoczęcia nie może być pusta.")]
         [DisplayName("Data rozpoczęcia")]
         [DataType(DataType.Date)]
         public DateTime Start { get; set; }
 
+        /// <summary>
+        /// Data zakończenia zadania.
+        /// </summary>
         [Required(ErrorMessage = "Data zakończenia nie może być pusta.")]
         [DisplayName("Data zakończenia")]
         [DataType(DataType.Date)]
         public DateTime End { get; set; }
 
+        /// <summary>
+        /// Priorytet zadania.
+        /// </summary>
         [Required(ErrorMessage = "Priorytet nie może być pusty.")]
         [DisplayName("Priorytet")]
         public int Priority { get; set; }
 
+        /// <summary>
+        /// Progres postępu zadania.
+        /// </summary>
         [Required(ErrorMessage = "Progres nie może być pusty.")]
         [DisplayName("Progres")]
         public int Progress { get; set; }
 
 
+        /// <summary>
+        /// Opis zadania.
+        /// </summary>
         [MaxLength(255, ErrorMessage = "Długość nie może przekraczać 255 znaków.")]
         [DisplayName("Opis")]
         public string Description { get; set; }
 
+        /// <summary>
+        /// Ścieżka do pliku dla zadania.
+        /// </summary>
         [DisplayName("Ścieżka do pliku")]
         public string FilePath { get; set; }
     }
